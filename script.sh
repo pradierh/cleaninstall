6193c2449110d5c9b6545f22594ed242d65a26bf
Hugo Pradier <83594897+pradierh@users.noreply.github.com>
2023-09-06 11:41:46 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -46,6 +46,13 @@ echo "Postman installed"
 sudo apt install qbittorrent -y
 echo "qBittorrent installed"
 
+# Install Onedrive
+wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_23.04/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_23.04/ ./" | sudo tee /etc/apt/sources.list.d/onedrive.list
+sudo apt update
+sudo apt install --no-install-recommends --no-install-suggests onedrive
+echo "Onedrive installed"
+
 # Install Fonts
 cp *.ttf ./fonts  # If the font files are in TTF format
 cp *.otf ./fonts  # If the font files are in OTF format
